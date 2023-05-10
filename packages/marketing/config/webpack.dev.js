370a9f102e3ee31da831fdff099dc93cca0570ac
akonshina <arina.konshina@teskalabs.com>
2023-05-10 11:42:41 +0200

    small updates

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,31 +1,33 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
-const packageJson = require("../package.json");
-
+const packageJson = require('../package.json');
 
 const devConfig = {
-	mode: 'development',
-	devServer: {
-		port: 7081,
-		historyApiFallback: {
-			index: '/index.html',
-		},
-	},
-	plugins: [
-		new ModuleFederationPlugin({
-			name: "marketing",
-			filename: "remoteEntry.js",
-			exposes: {
-				"./MarketingApp": "./src/bootstrap"
-			},
-			shared: packageJson.dependencies,
-		}),
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-		}),
-	],
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:7081/',
+  },
+  devServer: {
+    port: 7081,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
 
 module.exports = merge(commonConfig, devConfig);
