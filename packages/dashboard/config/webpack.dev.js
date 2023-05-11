b7df72a2d0ac711b736eb936bba1a2aeaa467543
akonshina <arina.konshina@teskalabs.com>
2023-05-11 10:01:39 +0200

    added dashboard

--- packages/dashboard/config/webpack.dev.js
+++ packages/dashboard/config/webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -6,23 +7,28 @@ const packageJson = require('../package.json');
 const devConfig = {
   mode: 'development',
   output: {
-    publicPath: 'http://localhost:7080/',
+    publicPath: 'http://localhost:7083/',
   },
   devServer: {
-    port: 7080,
+    port: 7083,
     historyApiFallback: {
       index: 'index.html',
     },
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: 'marketing@http://localhost:7081/remoteEntry.js',
-        auth: 'auth@http://localhost:7082/remoteEntry.js',
-        dashboard: 'dashboard@http://localhost:7083/remoteEntry.js',
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DashboardApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
     }),
   ],
 };
